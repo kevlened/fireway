72be87714451a7fb9cb40fed0b3668abf95bc233
Len Boyette <boyettel@gmail.com>
2021-04-07 12:28:37 -0700

    Fix types for FieldValue, FieldPath, and Timestamp
    resolves: #27

--- fireway.d.ts
+++ fireway.d.ts
@@ -1,8 +1,8 @@
 export interface MigrateOptions {
 	app: import("firebase-admin").app.App;
 	firestore: import("@google-cloud/firestore").Firestore;
-	FieldValue: import("@google-cloud/firestore").FieldValue;
-	FieldPath: import("@google-cloud/firestore").FieldPath;
-	Timestamp: import("@google-cloud/firestore").Timestamp;
+	FieldValue: typeof import("@google-cloud/firestore").FieldValue;
+	FieldPath: typeof import("@google-cloud/firestore").FieldPath;
+	Timestamp: typeof import("@google-cloud/firestore").Timestamp;
 	dryrun: boolean;
 }
